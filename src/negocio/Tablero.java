a691ada78a6efd815424549f2b303a617d8ddc97
jose forero <jmforeroc@udistrital.edu.co>
2023-11-20 21:47:37 -0500

    implementacion de turnos
    falta el movimiento y la ui

--- src/negocio/Tablero.java
+++ src/negocio/Tablero.java
@@ -19,7 +19,7 @@ public class Tablero {
 		this.tablero[4][4] = 1;
 	}	
 	
-	public void checkder(int coorx, int coory) {
+	public void checkder(int coorx, int coory, boolean turno) {
 
 		int pulpo = coory; 
 		int orca = coory;
@@ -29,40 +29,44 @@ public class Tablero {
 			System.out.print(this.tablero[coorx][coory] + "\n");
 				int i = 0;
 				do {
-					if(this.tablero [coorx][coory + 1] == 2) {
+					if(this.tablero [coorx][coory + 1] == 2 && turno || this.tablero [coorx][coory + 1] == 1 && !turno) {
 					if(coory + i + 1 < 8) {
 					if(this.tablero [coorx][coory + i + 1] == 0) {
 						System.out.println("\n movimiento posible");
-						this.tablero [coorx][coory + i + 1] = -1;
+						if (turno) {
+							this.tablero [coorx][coory + i + 1] = -1;
+						}else {
+							this.tablero [coorx][coory + i + 1] = -2;
+						}
 						nautilo = 1;
 						// poner salto al siguiente checkeo
 						coory = pulpo;
-						this.checkizq(coorx, coory);
+						this.checkizq(coorx, coory, turno);
 					}else {
 					i++;
 					}}
-					if(this.tablero [coorx][coory + i] == 1) {
+					if(this.tablero [coorx][coory + i] == 1 && turno || this.tablero [coorx][coory + i] == 2 && !turno) {
 						i++;
 					}
 					if(coory + i + 1 == 8) {
 						System.out.println("\n movimiento no posible");
 						// poner salto al siguiente checkeo
 						coory = pulpo;
-						this.checkizq(coorx, coory);
+						this.checkizq(coorx, coory, turno);
 					}
 					}else {
 							System.out.println("\n movimiento no posible");
 							// poner salto al siguiente checkeo
 							coory = pulpo;
-							this.checkizq(coorx, coory);
+							this.checkizq(coorx, coory, turno);
 					}
-				}while(this.tablero [coorx][coory + i] == 2);
+				}while(this.tablero [coorx][coory + i] == 2 && turno || this.tablero [coorx][coory + i] == 1 && !turno);
 				pulpo++;
 		}
 		coory = orca;
-		this.checkizq(coorx, coory);
+		this.checkizq(coorx, coory, turno);
 	}
-	public void checkizq(int coorx, int coory) {
+	public void checkizq(int coorx, int coory, boolean turno) {
 		int orca = coory;
 		int pulpo = coory; 
 		int nautilo = 0;
@@ -72,39 +76,43 @@ public class Tablero {
 			int i = 0;
 			System.out.print(this.tablero[coorx][coory - i ] + "\n");
 				do {
-					if(this.tablero[coorx][coory - 1] == 2) {
+					if(this.tablero[coorx][coory - 1] == 2 && turno || this.tablero[coorx][coory - 1] == 1 && !turno) {
 					if(coory - i - 1 >= 0) {
 					if(this.tablero [coorx][coory - i - 1] == 0) {
 						System.out.println("\n movimiento posible");
-						this.tablero [coorx][coory - i - 1] = -1;
+						if (turno) {
+							this.tablero [coorx][coory - i - 1] = -1;
+						}else {
+							this.tablero [coorx][coory - i - 1] = -2;
+						}
 						nautilo = 1;
 						// poner salto al siguiente checkeo
 						coory = pulpo;
-						this.checkarr(coorx, coory);
+						this.checkarr(coorx, coory, turno);
 						//this.checkarr(coorx, coory);
 					}else {
 					i++;
 					}}
-					if(this.tablero [coorx][coory - i] == 1) {
+					if(this.tablero [coorx][coory - i] == 1 && turno || this.tablero [coorx][coory - i] == 2 && !turno) {
 						i++;
 					}
 					if(coory - i - 1 < 0) {
 						System.out.println("\n movimiento no posible posible");
 						// poner salto al siguiente checkeo
-						this.checkarr(coorx, coory);
+						this.checkarr(coorx, coory, turno);
 					}
 					}else {
 						System.out.println("\n movimiento no posible");
 						// poner salto al siguiente checkeo
-						this.checkarr(coorx, coory);
+						this.checkarr(coorx, coory, turno);
 				}
-				}while(this.tablero [coorx][coory - i] == 2);
+				}while(this.tablero [coorx][coory - i] == 2 && turno || this.tablero [coorx][coory - i] == 1 && !turno);
 				pulpo--;
 		}
 		coory = orca;
-		this.checkarr(coorx, coory);
+		this.checkarr(coorx, coory, turno);
 	}
-	public void checkarr(int coorx, int coory) {
+	public void checkarr(int coorx, int coory, boolean turno) {
 		int orca = coorx;
 		int pulpo = coorx; 
 		int nautilo = 0;
@@ -114,40 +122,44 @@ public class Tablero {
 			int i = 0;
 			System.out.print(this.tablero[coorx - i ][coory] + "\n");
 				do {
-					if(this.tablero[coorx - 1][coory] == 2) {
+					if(this.tablero[coorx - 1][coory] == 2 && turno || this.tablero[coorx - 1][coory] == 1 && !turno) {
 					if(coorx - i - 1 >= 0) {
 					if(this.tablero [coorx - i - 1][coory] == 0) {
 						System.out.println("\n movimiento posible");
-						this.tablero [coorx - i - 1][coory] = -1;
+						if (turno) {
+							this.tablero [coorx - i - 1][coory] = -1;
+						}else {
+							this.tablero [coorx - i - 1][coory] = -2;
+						}
 						nautilo = 1;
 						// poner salto al siguiente checkeo
 						coorx = pulpo;
-						this.checkaba(coorx, coory);
+						this.checkaba(coorx, coory, turno);
 					}else {
 					i++;
 					}}
-					if(this.tablero [coorx - i][coory] == 1) {
+					if(this.tablero [coorx - i][coory] == 1 && turno || this.tablero [coorx - i][coory] == 2 && !turno) {
 						i++;
 					}
 					if(coorx - i - 1 < 0) {
 						System.out.println("\n movimiento no posible posible");
 						// poner salto al siguiente checkeo
 						coorx = pulpo;
-						this.checkaba(coorx, coory);
+						this.checkaba(coorx, coory, turno);
 					}
 					}else {
 						System.out.println("\n movimiento no posible posible");
 						// poner salto al siguiente checkeo
 						coorx = pulpo;
-						this.checkaba(coorx, coory);
+						this.checkaba(coorx, coory, turno);
 					}
-				}while(this.tablero [coorx - i][coory] == 2);
+				}while(this.tablero [coorx - i][coory] == 2 && turno || this.tablero [coorx - i][coory] == 1 && !turno);
 				pulpo--;
 		}
 		coorx = orca;
-		this.checkaba(coorx, coory);
+		this.checkaba(coorx, coory, turno);
 	}
-	public void checkaba(int coorx, int coory) {
+	public void checkaba(int coorx, int coory, boolean turno) {
 		int pulpo = coorx; 
 		int nautilo = 0;
 		//checkeo abajo
@@ -156,60 +168,68 @@ public class Tablero {
 			int i = 0;
 			System.out.print(this.tablero[coorx + i ][coory] + "\n");
 				do {
-					if(this.tablero[coorx + 1][coory] == 2) {
+					if(this.tablero[coorx + 1][coory] == 2 && turno || this.tablero[coorx + 1][coory] == 1 && !turno) {
 					if(coorx + i + 1 < 8) {
 					if(this.tablero [coorx + i + 1][coory] == 0) {
 						System.out.println("\n movimiento posible");
-						this.tablero [coorx + i + 1][coory] = -1;
+						if (turno) {
+							this.tablero [coorx + i + 1][coory] = -1;
+						}else {
+							this.tablero [coorx + i + 1][coory] = -2;
+						}
 						nautilo = 1;
 						// poner salto al siguiente checkeo
-						this.checkdsupder(coorx, coory);
+						this.checkdsupder(coorx, coory, turno);
 					}else {
 					i++;
 					}}
-					if(this.tablero [coorx + i][coory] == 1) {
+					if(this.tablero [coorx + i][coory] == 1 && turno || this.tablero [coorx + i][coory] == 2 && !turno) {
 						i++;
 					}
 					if(coorx + i + 1 == 8 ) {
 						System.out.println("\n movimiento no posible posible");
 						// poner salto al siguiente checkeo
-						this.checkdsupder(coorx, coory);
+						this.checkdsupder(coorx, coory, turno);
 					}
 					}else {
 						System.out.println("\n movimiento no posible posible");
 						// poner salto al siguiente checkeo
-						this.checkdsupder(coorx, coory);
+						this.checkdsupder(coorx, coory, turno);
 					}
-				}while(this.tablero [coorx + i][coory] == 2);
+				}while(this.tablero [coorx + i][coory] == 2 && turno || this.tablero [coorx + i][coory] == 1 && !turno);
 				pulpo++;
 		}
-		this.checkdsupder(coorx, coory);
+		this.checkdsupder(coorx, coory, turno);
 	}
-	public void checkdsupder(int coorx, int coory) {
+	public void checkdsupder(int coorx, int coory, boolean turno) {
 		int calamar = coorx;
 		int delfin = coorx;
 		int pulpo = coory; 
 		int orca = coory;
 		int nautilo = 0;
-		//checkeo deagonal superior izquierda
+		//checkeo deagonal superior derecha
 		while(pulpo < 8 && calamar >= 0 && nautilo == 0 && coory < 8 && coorx >= 0) {
 			System.out.print(this.tablero[coorx][coory] + "\n");
 				int i = 0;
 				do {
-					if(this.tablero [coorx - 1][coory + 1] == 2) {
+					if(this.tablero [coorx - 1][coory + 1] == 2 && turno || this.tablero [coorx - 1][coory + 1] == 1 && !turno) {
 					if(coory + i + 1 < 8 && coorx - i - 1 >= 0) {
 					if(this.tablero [coorx - i - 1][coory + i + 1] == 0) {
 						System.out.println("\n movimiento posible");
-						this.tablero [coorx - i - 1][coory + i + 1] = -1;
+						if (turno) {
+							this.tablero [coorx - i - 1][coory + i + 1] = -1;
+						}else {
+							this.tablero [coorx - i - 1][coory + i + 1] = -2;
+						}
 						nautilo = 1;
 						// poner salto al siguiente checkeo
 						coory = pulpo;
 						coorx = calamar;
-						this.checkdsupizq(coorx, coory);
+						this.checkdsupizq(coorx, coory, turno);
 					}else {
 					i++;
 					}}
-					if(this.tablero [coorx - i][coory + i] == 1) {
+					if(this.tablero [coorx - i][coory + i] == 1 && turno || this.tablero [coorx - i][coory + i] == 2 && !turno) {
 						i++;
 					}
 					if(coory + i + 1 == 8 && coorx - i - 1 < 0 || coorx - i - 1 < 0 || coory + i + 1 == 8) {
@@ -217,24 +237,24 @@ public class Tablero {
 						// poner salto al siguiente checkeo
 						coory = pulpo;
 						coorx = calamar;
-						this.checkdsupizq(coorx, coory);
+						this.checkdsupizq(coorx, coory, turno);
 					}
 					}else {
 							System.out.println("\n movimiento no posible");
 							// poner salto al siguiente checkeo
 							coory = pulpo;
 							coorx = calamar;
-							this.checkdsupizq(coorx, coory);
+							this.checkdsupizq(coorx, coory, turno);
 					}
-				}while(this.tablero [coorx - i][coory + i] == 2);
+				}while(this.tablero [coorx - i][coory + i] == 2 && turno || this.tablero [coorx - i][coory + i] == 1 && !turno);
 				pulpo++;
 				calamar--;
 		}
 		coory = orca;
 		coory = delfin;
-		this.checkdsupizq(coorx, coory);
+		this.checkdsupizq(coorx, coory, turno);
 	}
-	public void checkdsupizq(int coorx, int coory) {
+	public void checkdsupizq(int coorx, int coory, boolean turno) {
 		int calamar = coorx;
 		int delfin = coorx;
 		int pulpo = coory; 
@@ -244,19 +264,23 @@ public class Tablero {
 			System.out.print(this.tablero[coorx][coory] + "\n");
 				int i = 0;
 				do {
-					if(this.tablero [coorx - 1][coory - 1] == 2) {
+					if(this.tablero [coorx - 1][coory - 1] == 2 && turno || this.tablero [coorx - 1][coory - 1] == 1 && !turno) {
 					if(coory - i - 1 >= 0 && coorx - i - 1 >= 0) {
 					if(this.tablero [coorx - i - 1][coory - i - 1] == 0) {
 						System.out.println("\n movimiento posible");
-						this.tablero [coorx - i - 1][coory - i - 1] = -1;
+						if (turno) {
+							this.tablero [coorx - i - 1][coory - i - 1] = -1;
+						}else {
+							this.tablero [coorx - i - 1][coory - i - 1] = -2;
+						}
 						// poner salto al siguiente checkeo
 						coory = pulpo;
 						coorx = calamar;
-						this.checkdinfizq(coorx, coory);
+						this.checkdinfizq(coorx, coory, turno);
 					}else {
 					i++;
 					}}
-					if(this.tablero [coorx - i][coory - i] == 1) {
+					if(this.tablero [coorx - i][coory - i] == 1 && turno || this.tablero [coorx - i][coory - i] == 2 && !turno) {
 						i++;
 					}
 					if(coory - i - 1 < 0 && coorx - i - 1 < 0 || coorx - i - 1 < 0 || coory - i - 1 < 0) {
@@ -264,46 +288,50 @@ public class Tablero {
 						// poner salto al siguiente checkeo
 						coory = pulpo;
 						coorx = calamar;
-						this.checkdinfizq(coorx, coory);
+						this.checkdinfizq(coorx, coory, turno);
 					}
 					}else {
 							System.out.println("\n movimiento no posible");
 							// poner salto al siguiente checkeo
 							coory = pulpo;
 							coorx = calamar;
-							this.checkdinfizq(coorx, coory);
+							this.checkdinfizq(coorx, coory, turno);
 					}
-				}while(this.tablero [coorx - i][coory - i] == 2);
+				}while(this.tablero [coorx - i][coory - i] == 2 && turno || this.tablero [coorx - i][coory - i] == 1 && !turno);
 				pulpo--;
 				calamar--;
 		}
 		coory = orca;
 		coory = delfin;
-		this.checkdinfizq(coorx, coory);
+		this.checkdinfizq(coorx, coory, turno);
 	}
-	public void checkdinfizq(int coorx, int coory) {
+	public void checkdinfizq(int coorx, int coory, boolean turno) {
 		int calamar = coorx;
 		int delfin = coorx;
 		int pulpo = coory; 
 		int orca = coory;
-		//checkeo deagonal superior izquierda
+		//checkeo deagonal inferior izquierda
 		while(pulpo >= 0 && calamar < 8 || coory >= 0 && coorx < 8) {
 			System.out.print(this.tablero[coorx][coory] + "\n");
 				int i = 0;
 				do {
-					if(this.tablero [coorx + 1][coory - 1] == 2) {
+					if(this.tablero [coorx + 1][coory - 1] == 2 && turno || this.tablero [coorx + 1][coory - 1] == 1 && !turno) {
 					if(coory - i - 1 >= 0 && coorx + i + 1 < 8) {
 					if(this.tablero [coorx + i + 1][coory - i - 1] == 0) {
 						System.out.println("\n movimiento posible");
-						this.tablero [coorx + i + 1][coory - i - 1] = -1;
+						if (turno) {
+							this.tablero [coorx + i + 1][coory - i - 1] = -1;
+						}else {
+							this.tablero [coorx + i + 1][coory - i - 1] = -2;
+						}
 						// poner salto al siguiente checkeo
 						coory = pulpo;
 						coorx = calamar;
-						this.checkdinfder(coorx, coory);
+						this.checkdinfder(coorx, coory, turno);
 					}else {
 					i++;
 					}}
-					if(this.tablero [coorx + i][coory - i] == 1) {
+					if(this.tablero [coorx + i][coory - i] == 1 && turno || this.tablero [coorx + i][coory - i] == 2 && !turno) {
 						i++;
 					}
 					if(coory - i - 1 < 0 && coorx + i + 1 == 8 || coorx + i + 1 == 8 || coory - i - 1 < 0) {
@@ -311,24 +339,24 @@ public class Tablero {
 						// poner salto al siguiente checkeo
 						coory = pulpo;
 						coorx = calamar;
-						this.checkdinfder(coorx, coory);
+						this.checkdinfder(coorx, coory, turno);
 					}
 					}else {
 							System.out.println("\n movimiento no posible");
 							// poner salto al siguiente checkeo
 							coory = pulpo;
 							coorx = calamar;
-							this.checkdinfder(coorx, coory);
+							this.checkdinfder(coorx, coory, turno);
 					}
-				}while(this.tablero [coorx + i][coory - i] == 2);
+				}while(this.tablero [coorx + i][coory - i] == 2 && turno || this.tablero [coorx + i][coory - i] == 1 && !turno);
 				pulpo--;
 				calamar++;
 		}
 		coory = orca;
 		coory = delfin;
-		this.checkdinfder(coorx, coory);
+		this.checkdinfder(coorx, coory, turno);
 	}
-	public void checkdinfder(int coorx, int coory) {
+	public void checkdinfder(int coorx, int coory, boolean turno) {
 		int calamar = coorx;
 		int delfin = coorx;
 		int pulpo = coory; 
@@ -339,11 +367,15 @@ public class Tablero {
 			System.out.print(this.tablero[coorx][coory] + "\n");
 				int i = 0;
 				do {
-					if(this.tablero [coorx + 1][coory + 1] == 2) {
+					if(this.tablero [coorx + 1][coory + 1] == 2 && turno || this.tablero [coorx + 1][coory + 1] == 1 && !turno) {
 					if(coory + i + 1 < 8 && coorx + i + 1 < 8) {
 					if(this.tablero [coorx + i + 1][coory + i + 1] == 0) {
 						System.out.println("\n movimiento posible");
-						this.tablero [coorx + i + 1][coory + i + 1] = -1;
+						if (turno){
+							this.tablero [coorx + i + 1][coory + i + 1] = -1;
+						}else {
+							this.tablero [coorx + i + 1][coory + i + 1] = -2;
+						}
 						nautilo = 1;
 						// poner salto al siguiente checkeo
 						coory = pulpo;
@@ -353,7 +385,7 @@ public class Tablero {
 					}else {
 					i++;
 					}}
-					if(this.tablero [coorx + i][coory + i] == 1) {
+					if(this.tablero [coorx + i][coory + i] == 1 && turno || this.tablero [coorx + i][coory + i] == 2 && !turno) {
 						i++;
 					}
 					if(coory + i + 1 == 8 && coorx + i + 1 == 8 || coorx + i + 1 == 8 || coory + i + 1 == 8) {
@@ -372,7 +404,7 @@ public class Tablero {
 							this.print();
 							System.exit(0);
 					}
-				}while(this.tablero [coorx + i][coory + i] == 2);
+				}while(this.tablero [coorx + i][coory + i] == 2 && turno || this.tablero [coorx + i][coory + i] == 1 && !turno);
 				pulpo++;
 				calamar++;
 		}
@@ -397,5 +429,13 @@ public class Tablero {
             }
             System.out.println();
         }       
+	}
+	public boolean cambioturno(boolean turno) {
+		if(turno) {
+			turno = false;
+		}else {
+			turno = true;
+		}
+		return turno;
 	}
 }
